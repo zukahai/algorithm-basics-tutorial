d619019d44569684718163c8125fc964af1bff87
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-04-24 11:23:45 +0700

    inital: add letcode folder

---
+++ letcode/longest-valid-parentheses.js
@@ -0,0 +1,54 @@
+//  https://leetcode.com/problems/longest-valid-parentheses
+
+var longestValidParentheses = function (s) {
+    let max = 0;
+    let value = 0;
+    let countOpen = 0;
+    for (let i = 0; i < s.length; i++) {
+        value += (s[i] == '(') ? 1 : -1;
+        countOpen += (s[i] == ')') ? 1 : 0;
+        if (value >= 0) {
+            if (max < 2 * countOpen)
+                max = 2 * countOpen;
+        } else {
+            countOpen = 0;
+        }
+        console.log(countOpen, value);
+    }
+      
+    return max;
+};
+
+const s = "(())()(()((";
+console.log((longestValidParentheses(s)));
+
+
+
+// var longestValidParentheses = function (s) {
+//     let a = [];
+//     let first = new Map();
+//     let last = new Map();
+//     if (s[0] == '(')
+//         first.set(0, -1);
+
+//     let value = 0;
+//     for (let i = 0; i < s.length; i++) {
+//         value += (s[i] == '(') ? 1 : -1;
+//         a.push(value);
+//         if (!first.has(value) && s[i + 1] == '(')
+//             first.set(value, i);
+//         if (s[i] == ')')
+//             last.set(value, i);
+//     }
+//     let max = 0;
+//     last.forEach((value, key, last) => {
+//         let indexFirst = first.get(key);
+//         let indexLast = value;
+//         if (indexLast - indexFirst > max && s[indexFirst + 1] == '(' && s[indexLast] == ')')
+//             max = indexLast - indexFirst;
+//     });
+//     console.log(first);
+//     console.log(last);
+//     console.log(a);
+//     return max;
+// };
