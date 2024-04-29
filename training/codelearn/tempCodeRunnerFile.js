8d237fa242fcad4892b60554391a18cb9e4859cd
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-04-29 20:59:10 +0700

    Refactor code in day_07 directory

---
+++ training/codelearn/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+or(let i = 0; i < matrix.length; i++){
+    //     for(let j = 0; j < matrix[0].length; j++){
+    //         if(matrix[i][j] === 0 && flag[i][j] === true){
+    //             count++;
+    //             loang(i, j);
+    //         }
+    //     }
+    // }
