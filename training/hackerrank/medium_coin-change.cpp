e9cd24f9be2b65ca67977f44b5fa9ff276121a10
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-06-02 21:01:06 +0700

    training: add problem coin-change in hackerrank

--- training/hackerrank/medium_coin-change.cpp
+++ training/hackerrank/medium_coin-change.cpp
@@ -2,9 +2,40 @@
 
 using namespace std;
 
-int a[259];
-int dp[59];
+long getWays(int n, vector<long> c) {
+    vector<long> ways(n + 1, 0);
+    ways[0] = 1;
+    for (int i = 0; i < c.size(); i++) {
+        for (int j = c[i]; j <= n; j++) {
+            ways[j] += ways[j - c[i]];
+        }
+    }
+    return ways[n];
+}
 
 int main() {
-    cout << "a";
+    int n;
+    int m;
+    cin >> n >> m;
+    vector<long> c(m);
+    for (int i = 0; i < m; i++) {
+        cin >> c[i];
+    }
+    long ways = getWays(n, c);
+    cout << ways << "\n";
+    return 0;
 }
+
+/*
+7 4
+2 3 5 7 
+
+0: 1
+1: 0
+2: 1
+3: 1
+4: 1
+5: 2
+6: 2
+7: 3
+*/
