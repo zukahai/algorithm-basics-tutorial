baacb04dd172c0b195cfd76ecf108585c5c338a1
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-06-04 18:46:17 +0700

    refactor: optimize area calculation in CPL0A.cpp

--- training/bkdnoj/100_CPL0A.cpp
+++ training/bkdnoj/100_CPL0A.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 double area(int x1, int y1, int x2, int y2, int x3, int y3) {
-    return abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2.0;
+    return abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2;
 }
 
 bool checkInTriangle(int x1, int y1, int x2, int y2, int x3, int y3, int xTest, int yTest) {
@@ -11,7 +11,7 @@ bool checkInTriangle(int x1, int y1, int x2, int y2, int x3, int y3, int xTest, 
     double area1 = area(x1, y1, x2, y2, xTest, yTest);
     double area2 = area(x2, y2, x3, y3, xTest, yTest);
     double area3 = area(x3, y3, x1, y1, xTest, yTest);
-    return areaSum = (area1 + area2 + area3);
+    return areaSum == (area1 + area2 + area3);
 }
 
 int main() {
@@ -24,10 +24,8 @@ int main() {
         cin >> xTest >> yTest;
         count += checkInTriangle(x1, y1, x2, y2, x3, y3, xTest, yTest);
     }
-    double areaSum = area(x1, y1, x2, y2, x3, y3) + 0.00001;
-    double areaSum2 = area(x1, y1, x2, y2, x3, y3);
    
-    long long a10 = areaSum * 10;
+    long long a10 = area(x1, y1, x2, y2, x3, y3) * 10;
     cout << fixed << setprecision(1) << a10 / 10.0 << endl;
     cout << count;
 }
