962ae39b64b793b917f4de082bdd005403ed4a36
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-06-04 18:10:55 +0700

    training: add problem easy_longest-palindrome in letcode

---
+++ training/bkdnoj/100_CPL0A.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+double area(int x1, int y1, int x2, int y2, int x3, int y3) {
+    double a = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    double b = sqrt((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3));
+    double c = sqrt((x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1));
+    double p = (a + b + c) / 2;
+    return sqrt(p * (p - a) * (p - b) * (p - c));
+}
+
+bool checkInTriangle(int x1, int y1, int x2, int y2, int x3, int y3, int xTest, int yTest) {
+    double areaSum = area(x1, y1, x2, y2, x3, y3);
+    double area1 = area(x1, y1, x2, y2, xTest, yTest);
+    double area2 = area(x2, y2, x3, y3, xTest, yTest);
+    double area3 = area(x3, y3, x1, y1, xTest, yTest);
+    return abs(areaSum - (area1 + area2 + area3)) < 0.00001;
+}
+
+int main() {
+    int x1, y1, x2, y2, x3, y3, xTest, yTest;
+    int n;
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+    cin >> n;
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> xTest >> yTest;
+        count += checkInTriangle(x1, y1, x2, y2, x3, y3, xTest, yTest);
+    }
+    double areaSum = area(x1, y1, x2, y2, x3, y3) + 0.00001;
+    double areaSum2 = area(x1, y1, x2, y2, x3, y3);
+   
+    long long a10 = areaSum * 10;
+    cout << fixed << setprecision(1) << a10 / 10.0 << endl;
+    cout << count;
+}
