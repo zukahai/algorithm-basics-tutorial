642f15038c1add8c8cbb56f0518c6bbfc14476dc
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-06-05 14:17:23 +0700

    training: add new document cpp

---
+++ training/letcode/10000_size_t.cpp
@@ -0,0 +1,19 @@
+/*
+https://tleoj.edu.vn/problem/edu019a
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string s = "abc";
+    int n = -1;
+    if (s.size() > n) {
+        cout << "1" << endl;
+    }
+    if (s.size() - n > 0){
+        cout << "2" << endl;
+    }
+}
