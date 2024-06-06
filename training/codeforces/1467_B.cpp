4cba9e5dd208fc531b9bfdd183fc003dcbcee5cd
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-06-07 02:21:21 +0700

    traning: add problem 1467_B in codeforces

---
+++ training/codeforces/1467_B.cpp
@@ -0,0 +1,53 @@
+/*
+https://codeforces.com/contest/1467/problem/A
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+const int N = 3e5;
+int a[N], n;
+
+int isValley(int i) {
+    return (i > 0 && i < n - 1 && a[i] < a[i - 1] && a[i] < a[i + 1]);
+}
+
+int isHill(int i) {
+    return (i > 0 && i < n - 1 && a[i] > a[i - 1] && a[i] > a[i + 1]);
+}
+
+int solveTestCase() {
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    int is[n] = {};
+    int s = 0;
+    for (int i = 1; i < n - 1; i++) {
+        if (isHill(i) || isValley(i))
+            is[i] = 1, s++;
+    }
+
+    int ans = s;
+    for (int i = 1; i < n - 1; i++) {
+        int temp = a[i];
+        a[i] = a[i - 1];
+        ans = min(ans, s - is[i - 1] - is[i] - is[i + 1] + isHill(i - 1) + isValley(i - 1) + isHill(i) + isValley(i) + isHill(i + 1) + isValley(i + 1));
+        a[i] = a[i + 1];
+        ans = min(ans, s - is[i - 1] - is[i] - is[i + 1] + isHill(i - 1) + isValley(i - 1) + isHill(i) + isValley(i) + isHill(i + 1) + isValley(i + 1));
+        a[i] = temp;
+    }
+
+    cout << ans << "\n";
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solveTestCase();
+}
