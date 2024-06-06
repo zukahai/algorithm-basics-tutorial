0e47953aa10ff7a8fc483858625073ae32a0e6cd
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-06-07 02:50:44 +0700

    traning: add problem 1455_C in codeforces

---
+++ training/codeforces/1455_C.cpp
@@ -0,0 +1,18 @@
+/*
+https://codeforces.com/contest/1455/problem/C
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+	int x, y;
+	cin >> x >> y;
+	cout << x - 1 << " " << y << endl;	
+}
+
+int main() {
+	int t;
+	cin >> t;
+	while (t--) solve();
+}
