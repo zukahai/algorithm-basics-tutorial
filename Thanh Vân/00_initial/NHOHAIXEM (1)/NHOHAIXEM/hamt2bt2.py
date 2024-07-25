9e4b598bd9a1066a51b81cb4f8a8f2efbd0db684
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-07-25 23:00:05 +0700

    Thanh Vân/00_initial/NHOHAIXEM (1)/NHOHAIXEM/hamt2bt2.py

---
+++ Thanh Vân/00_initial/NHOHAIXEM (1)/NHOHAIXEM/hamt2bt2.py
@@ -0,0 +1,16 @@
+from math import *
+def nguyento(n):
+   if n<2:
+       return False
+   for i in range(2,isqrt(n)+1):
+        if n%i==0:
+            return False
+   return True
+#main
+n=int(input("nhap n: "))
+d=0
+for i in range(1,isqrt(n)+1):
+    if n%i==0 and nguyento(i):
+        d+=1
+print(d)
+            
