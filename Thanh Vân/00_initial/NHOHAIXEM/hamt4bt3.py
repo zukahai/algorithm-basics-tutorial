ea7d491e8ebafa20101da08d3c52c0c7df8e209d
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-08-02 20:11:50 +0700

    Thanh Vân/00_initial/NHOHAIXEM/hamt4bt3.py

--- Thanh Vân/00_initial/NHOHAIXEM/hamt4bt3.py
+++ Thanh Vân/00_initial/NHOHAIXEM/hamt4bt3.py
@@ -6,11 +6,21 @@ def nguyento(n):
         if n%i==0:
             return False
     return True
+
+def isCp(n):
+    return isqrt(n)**2 == n
+
 def ntrg(n):
+    # 9: 1 9 3 3
     s=0
-    for i in range(2,n+1):
-        if n%i==0 and nguyento(i):
-         s+=i
+    for i in range(1, isqrt(n)+1):
+        if n%i==0:
+            if nguyento(i):
+                s+=i
+            if nguyento(n//i):
+                s+=n//i
+    if isCp(n) and nguyento(isqrt(n)):
+        s -= isqrt(n)
     return s
 #main
 n=int(input("hay nhap n "))
