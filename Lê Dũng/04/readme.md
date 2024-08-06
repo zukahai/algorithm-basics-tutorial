573cf41301be5ca62a573d77e5de336132e84c2d
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-08-06 21:43:34 +0700

    Refactor code in problems/SoSieuNguyenTo/generator/generator.h

---
+++ Lê Dũng/04/readme.md
@@ -0,0 +1,34 @@
+# Bài 1: Số giảm dần
+
+**Số giảm dần** là số mà các chữ số trong số đó giảm dần từ trái sang phải. Ví dụ: 321, 97531, 21, 543210 là số giảm dần.
+
+Tìm số giảm dần nhỏ nhất lớn hơn hoặc bằng số nguyên dương `n` cho trước.
+
+**Input**:
+
+- Một số nguyên `n` (1 ≤ `n` ≤ 10^18).
+
+**Output**:
+
+- Đưa ra số giảm dần nhỏ nhất lớn hơn hoặc bằng `n`.
+
+### Ví dụ
+
+#### Ví dụ 1:
+
+| Input | Output |
+|-------|--------|
+| 34     | 40     |
+
+#### Ví dụ 2:
+
+| Input | Output |
+|-------|--------|
+| 436     | 510     |
+
+#### Ví dụ 3:
+
+| Input | Output |
+|-------|--------|
+| 999     | 3210     |
+
---
+++ Lê Dũng/04/soDoiXung.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string reverse(string s) {
+    string res = "";
+    for (int i = s.length() - 1; i >= 0; i--) {
+        res += s[i];
+    }
+    return res;
+}
+
+string generatePalindrome(string s, int type) {
+    if (type == 1)
+        return s + reverse(s);
+    return s + reverse(s.substr(0, s.length() - 1));
+}
+
+bool isCompare(string s, string p) {
+    while (s.length() < p.length()) 
+        s = "0" + s;
+    while (s.length() > p.length()) 
+        p = "0" + p;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] != p[i]) {
+            return s[i] > p[i];
+        }
+    }
+    return true;
+}
+
+string sum(string s, string p) {
+    while (s.length() < p.length())
+        s = "0" + s;
+    while (s.length() > p.length()) 
+        p = "0" + p;
+    string res = "";
+    int n = s.length();
+    int carry = 0;
+    for (int i = n - 1; i >= 0; i--) {
+        int sum = (s[i] - '0') + (p[i] - '0') + carry;
+        res = char(sum % 10 + '0') + res;
+        carry = sum / 10;
+    }
+    if (carry > 0) 
+        res = char(carry + '0') + res;
+    return res;
+}
+
+int main() {
+    string s;
+    getline(cin, s);
+    int n = s.length();
+    string p = s.substr(0, n / 2);
+    int type = 1;
+    if (n % 2 != 0) {
+        type = 2;
+        p = s.substr(0, n / 2 + 1);
+    }
+
+    string palindrome = generatePalindrome(p, type);
+    if (isCompare(palindrome, s)) {
+        cout << palindrome;
+    } else {
+        cout << generatePalindrome(sum(p, "1"), type);
+    }
+}
