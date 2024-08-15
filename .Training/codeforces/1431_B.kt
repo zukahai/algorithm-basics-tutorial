99543a2eee4e5a031582dfd68cd3cd9c914d8514
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-08-15 21:14:11 +0700

    Added tests for .Training/codeforces/1431_B.kt

---
+++ .Training/codeforces/1431_B.kt
@@ -0,0 +1,7 @@
+fun main() {
+    repeat(readLine()!!.toInt()) {
+        val s = readLine()!!
+        val answer = s.count { it == 'w' } + s.split("w").sumOf { it.length / 2 }
+        println(answer)
+    }
+}
