cbf359dc0ee7050760508a506214261cfa96c4a4
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-08-18 21:19:43 +0700

    chore: Refactor code for finding the nth digit in a sequence

---
+++ Lê Dũng/08_quy_hoạch_động_2/DayConLienTiepCoTongLonNhat.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    vector<int> a;
+    int n;
+    cin >> n;
+    a.resize(n);
+    vector<int> f(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    f[0] = a[0];
+
+    for (int i = 1; i < n; i++) {
+        if (f[i - 1] > 0) {
+            f[i] = f[i - 1] + a[i];
+        } else {
+            f[i] = a[i];
+        }
+    }
+
+    int max = f[0];
+    int csMax = 0;
+
+    for (int i = 1; i < n; i++) {
+        if (f[i] > max) {
+            max = f[i];
+            csMax = i;
+        }
+    }
+
+    vector<int> res;
+
+    while(max != 0) {
+        res.push_back(a[csMax]);
+        max -= a[csMax];
+        csMax--;
+    }
+
+    for (int i = res.size() - 1; i >= 0; i--) {
+        cout << res[i] << " ";
+    }
+}
