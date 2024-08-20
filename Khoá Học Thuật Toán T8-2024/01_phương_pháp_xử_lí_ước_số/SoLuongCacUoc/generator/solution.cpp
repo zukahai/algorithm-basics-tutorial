5c8716f27f4ad17437d200d15f4297f684c4e703
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-08-21 03:38:36 +0700

    Reorganized project structure with SoLuongCacUoc/generator/solution.cpp

---
+++ Khoá Học Thuật Toán T8-2024/01_phương_pháp_xử_lí_ước_số/SoLuongCacUoc/generator/solution.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ifstream cin("input.txt");
+    ofstream cout("output.txt");
+
+    long long n;
+    cin >> n;
+    int count = 0;
+
+    for (int i = 1; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            count += 2;
+        }
+    }
+
+    if ((int)sqrt(n) == sqrt(n)) {
+        count--;
+    }
+
+    cout << count;
+}
