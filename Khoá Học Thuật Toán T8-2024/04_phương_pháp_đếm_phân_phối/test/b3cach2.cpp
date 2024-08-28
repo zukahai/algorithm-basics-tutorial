2dd486733429af3b93d957c3a036819f848440b2
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-08-28 21:37:32 +0700

    chore: Refactor code to improve efficiency and readability

--- Khoá Học Thuật Toán T8-2024/04_phương_pháp_đếm_phân_phối/test/b3cach2.cpp
+++ Khoá Học Thuật Toán T8-2024/04_phương_pháp_đếm_phân_phối/test/b3cach2.cpp
@@ -12,10 +12,12 @@ int main() {
     cin >> n;
     vector <int> a(n);
 
+    // Nhập dãy số
     for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
 
+    // Tìm số lớn nhất trong dãy
     int max = a[0];
     for (int i = 1; i < n; i++) {
         if (a[i] > max) {
@@ -25,10 +27,13 @@ int main() {
 
     vector<int> b(max + 1, 0);
 
+    // Gặp số a[i] thì tăng b[a[i]] lên 1
+    // Vì số a[i] đã xuất hiện thêm 1 lần
     for (int i = 0; i < n; i++) {
         b[a[i]]++;
     }
 
+    // In ra số lần xuất hiện của các số từ 1 đến max
     for (int i = 1; i <= max; i++) {
         if (b[i] != 0)
             cout << i << " " << b[i] << endl;
