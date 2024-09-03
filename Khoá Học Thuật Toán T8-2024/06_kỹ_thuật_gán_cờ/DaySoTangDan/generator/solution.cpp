df66e7b3476ff6d5d84bd464e50c711396f21dd7
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-09-03 13:44:12 +0700

    Enhanced user experience for DaySoTangDan/generator/solution.cpp

---
+++ Khoá Học Thuật Toán T8-2024/06_kỹ_thuật_gán_cờ/DaySoTangDan/generator/solution.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ifstream cin("input.txt");
+    ofstream cout("output.txt");
+
+    int n;
+    cin >> n;
+    vector <int> a(n);
+
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    string flag = "YES";
+
+    for (int i = 1; i < n; i++)
+        if (a[i] <= a[i - 1])
+        {
+            flag = "NO";
+            break;
+        }
+
+    cout << flag;
+    
+}
