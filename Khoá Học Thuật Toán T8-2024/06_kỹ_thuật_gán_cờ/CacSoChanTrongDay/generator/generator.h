a629762f9c01cd1f4176e5cdfd1f211f0c03f29c
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-09-03 12:49:59 +0700

    Documented changes in CacSoChanTrongDay/generator/generator.h

---
+++ Khoá Học Thuật Toán T8-2024/06_kỹ_thuật_gán_cờ/CacSoChanTrongDay/generator/generator.h
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#include "../lib/library.h"
+using namespace std;
+
+void gen(int iTest, int testnum, string target_file)
+{
+    ofstream cout(target_file);
+    
+    // Cout ra những input cần thiết
+    int n = 2 * iTest + random(1, 10);
+
+    if (iTest >= testnum - 1)
+        n = 1000000;
+    
+    vector <int> a = random_vector(n, -1000, 1000);
+
+    if (random(0, 2) == 0)
+        for (int i = 0; i < n; i++)
+            a[i] = (a[i] / 2) * 2 + 1;
+
+    cout << n << endl;
+    print_vector(a, cout);
+}
