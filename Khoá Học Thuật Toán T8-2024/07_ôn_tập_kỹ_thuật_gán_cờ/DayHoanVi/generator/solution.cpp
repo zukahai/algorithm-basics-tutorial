9cdaca31629e96a0ea6ffade8c55d2a7a74e9ebe
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-09-06 22:02:50 +0700

    Reorganized project structure with DayHoanVi/generator/solution.cpp

---
+++ Khoá Học Thuật Toán T8-2024/07_ôn_tập_kỹ_thuật_gán_cờ/DayHoanVi/generator/solution.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ifstream cin("input.txt");
+    ofstream cout("output.txt");
+
+    int n;
+    cin >> n;
+    vector <int> a(n);
+    for(int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    for (int i = 0; i < n; i++)
+    {
+        if (i != a[i] - 1)
+        {
+            cout << "NO" << endl;
+            return 0;
+        }
+    }
+    cout << "YES" << endl;
+}
