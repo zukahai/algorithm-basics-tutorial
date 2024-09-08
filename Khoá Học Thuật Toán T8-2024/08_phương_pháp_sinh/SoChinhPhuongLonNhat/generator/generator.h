d49698f8c1fa83142852a819a9be3563beb4d707
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-09-08 21:43:53 +0700

    Added new functionality to SoChinhPhuongLonNhat/generator/generator.h

---
+++ Khoá Học Thuật Toán T8-2024/08_phương_pháp_sinh/SoChinhPhuongLonNhat/generator/generator.h
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#include "../lib/library.h"
+using namespace std;
+
+void gen(int iTest, int testnum, string target_file)
+{
+    ofstream cout(target_file);
+    
+    // Cout ra những input cần thiết
+
+    long long k = iTest * 2000 + random(1ll, iTest);
+    k = k * k * k;
+    if (iTest == testnum)  {
+        k = 1e18;
+    }
+    if (iTest < 10)
+        k = iTest;
+
+    cout << k;
+    
+}
