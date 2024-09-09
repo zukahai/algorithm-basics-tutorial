cb624f284c48adcdaf881555d49b5dafa8e1b2dc
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-09-09 22:38:57 +0700

    Removed deprecated code from SoSieuDoiXungEz/generator/generator.h

---
+++ Khoá Học Thuật Toán T8-2024/08_phương_pháp_sinh/SoSieuDoiXungEz/generator/generator.h
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#include "../lib/library.h"
+using namespace std;
+
+void gen(int iTest, int testnum, string target_file)
+{
+    ofstream cout(target_file);
+    
+    // Cout ra những input cần thiết
+    if ((float)iTest / testnum <= 0.3) {
+        cout << random_string(1, "123456789") + random_string(4, "0123456789") + "\n";
+    } else {
+        cout << random_string(1, "123456789") + random_string(16, "0123456789") + "\n";
+    }
+    
+}
