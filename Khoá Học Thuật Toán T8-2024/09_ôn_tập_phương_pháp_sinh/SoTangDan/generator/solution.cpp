dee6f050f56e4a658d0de2d6c4c08e3e0be0f153
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-09-11 21:42:18 +0700

    Updated implementation of SoTangDan/generator/solution.cpp

---
+++ Khoá Học Thuật Toán T8-2024/09_ôn_tập_phương_pháp_sinh/SoTangDan/generator/solution.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ifstream cin("input.txt");
+    ofstream cout("output.txt");
+
+    long long n;
+    cin >> n;
+    vector<long long> a = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    int index = 0;
+
+    while (a[a.size() - 1] <= n && index < a.size())
+    {
+        long long temp = a[index];
+        int lastDigit = temp % 10;
+        for (int i = lastDigit + 1; i <= 9; i++)
+        {
+            long long newNum = temp * 10 + i;
+            a.push_back(newNum);
+        }
+        index++;
+    }
+
+    for (int i = 0; i < a.size(); i++)
+    {
+        if (a[i] > n)
+            break;
+        cout << a[i] << " ";
+    }
+
+    
+}
