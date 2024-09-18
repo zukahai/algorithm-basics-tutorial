3c79098eea3e1a91a03d41ee2fd728677f1d5c34
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-09-18 14:51:27 +0700

    Revised implementation of TongLuyThuaBac5/generator/generator.h

---
+++ Khoá Học Thuật Toán T8-2024/11_các_công_thức_số_học/TongLuyThuaBac5/generator/generator.h
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#include "../lib/library.h"
+using namespace std;
+
+void gen(int iTest, int testnum, string target_file)
+{
+    ofstream cout(target_file);
+    
+    // Cout ra những input cần thiết
+
+    vector <int> lensub = {3, 6, 18};
+
+    string s = "";
+    int k = 1;
+
+    if (iTest * 100 < testnum * 20)
+    {
+        k = 0;
+        
+    }
+    else if (iTest * 100 < testnum * 40)
+    {
+        k = 1;
+    } else {
+        // Sinh test ngẫu nhiên
+       k = 2;
+    }
+
+    int len = lensub[k] * (iTest * 1.0 / testnum);
+    if (len == 0) len = 1;
+
+    s = random_string(1, "123456789") + random_string(len, "0123456789");
+
+    if (iTest == testnum)
+    {
+        s = "1000000000000000000";
+    }
+
+    cout << s;
+}
