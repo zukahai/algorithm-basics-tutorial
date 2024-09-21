1ee224f4c9eddd7f799600a7aaa9f49fb146866d
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-09-21 19:46:52 +0700

    HaiZuka/commit_and_push.sh

--- HaiZuka/commit_and_push.sh
+++ HaiZuka/commit_and_push.sh
@@ -1,5 +1,5 @@
 # Đổi thư mục làm việc tới thư mục chứa repo Git của bạn
-Set-Location -Path "G:\algorithm-mastery-in-30-days/Khoá Học Thuật Toán T8-2024/11_các_công_thức_số_học/"
+Set-Location -Path "G:\algorithm-mastery-in-30-days/Khoá Học Thuật Toán T8-2024/12_kỳ_thi_giữa_khoá/"
 # Set-Location -Path "G:\algorithm-mastery-in-30-days/Phạm Thành An/02_quy_hoạch_động_1/"
 
 # Lấy danh sách tất cả các tệp tin chưa được theo dõi từ git status
