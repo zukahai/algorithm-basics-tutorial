86f524a16c6ed9e102ceaaba7a3c517aa79b0870
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-10-05 10:31:15 +0700

    Added tests for TroChoiRutSo2_2/generator/generator.h

---
+++ Khoá Học Thuật Toán T8-2024/18_quy_hoạch_động_5/TroChoiRutSo2_2/generator/generator.h
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+#include "../lib/library.h"
+using namespace std;
+
+void gen(int iTest, int testnum, string target_file)
+{
+    ofstream cout(target_file);
+    
+    // Cout ra những input cần thiết
+
+    vector <int> lensub = {1, 2, 17};
+
+    string s = "";
+    int k = 1;
+
+    if (iTest * 100 < testnum * 20)
+    {
+        k = 0;
+        
+    }
+    else if (iTest * 100 < testnum * 40)
+    {
+        k = 1;
+    } else {
+        // Sinh test ngẫu nhiên
+       k = 2;
+    }
+
+    int len = lensub[k] * (iTest * 1.0 / testnum);
+    if (len == 0) len = 1;
+
+    s = random_string(1, "123456789") + random_string(len - 1, "0123456789");
+
+    long long n = stoll(s);
+
+    if (iTest >= testnum - 10)
+    {
+        n = 1e18 - random(1, 1000);
+    }
+
+    long long k1 = random(1, n);
+    if (random(0, 3) == 0) {
+        k1 += n;
+    }
+
+
+    if (random(0, 1) == 1)
+    {
+        n = ((n + k1) / (k1 + 1)) * (k1 + 1);
+    }
+
+    
+    cout << n << " " << k1;
+}
