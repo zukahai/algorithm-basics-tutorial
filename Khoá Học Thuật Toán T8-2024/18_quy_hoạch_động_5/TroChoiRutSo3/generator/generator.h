bef0b1d4263c277c81001b5f6649209ec9a10773
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-10-05 10:39:03 +0700

    Resolved bugs in TroChoiRutSo3/generator/generator.h

---
+++ Khoá Học Thuật Toán T8-2024/18_quy_hoạch_động_5/TroChoiRutSo3/generator/generator.h
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+#include "../lib/library.h"
+using namespace std;
+
+void gen(int iTest, int testnum, string target_file)
+{
+    ofstream cout(target_file);
+    
+    // Cout ra những input cần thiết
+
+    vector <int> lensub = {1, 2, 5};
+
+    string s = "";
+    int k = 1;
+
+    if (iTest * 100 < testnum * 20)
+    {
+        k = 0;
+        
+    }
+    else if (iTest * 100 < testnum * 40)
+    {
+        k = 1;
+    } else {
+        // Sinh test ngẫu nhiên
+       k = 2;
+    }
+
+    int len = lensub[k] * (iTest * 1.0 / testnum);
+    if (len == 0) len = 1;
+
+    s = random_string(1, "123456789") + random_string(len - 1, "0123456789");
+
+    long long n = stoll(s);
+
+    if (iTest >= testnum - 10)
+    {
+        n = 1e5- random(1, 1000);
+    }
+
+
+    vector <bool> dp(n + 1);
+    dp[0] = false;
+    for (int i = 1; i <= n; i++)
+    {
+        dp[i] = false;
+        for (int j = 1; j * j <= i; j++) {
+            if (dp[i - j * j] == false) {
+                dp[i] = true;
+                break;
+            }
+        }
+    }
+
+    int m = random(0, 1);
+    int index = n;
+    while (index > 1 && dp[index] != m) index --;
+    cout << index;
+}
