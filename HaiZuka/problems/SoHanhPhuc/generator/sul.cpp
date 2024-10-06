81481c84300b4f0b5c06b77158d4c01fa91b50c6
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-10-07 03:13:23 +0700

    Added tests for problems/SoHanhPhuc/generator/sul.cpp

---
+++ HaiZuka/problems/SoHanhPhuc/generator/sul.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector <int> mp(10000009, -1);
+
+int sumDigitSquare(int n) {
+    int sum = 0;
+    while (n > 0) {
+        sum += (n % 10) * (n % 10);
+        n /= 10;
+    }
+    return sum;
+}
+
+void solve(int n) {
+    bool flag = false;
+    if (mp[n] != -1)
+        return;
+    unordered_set <int> s;
+    s.insert(n);
+    while(true) {
+        n = sumDigitSquare(n);
+        if (mp[n] != -1) {
+            flag = mp[n];
+            break;
+        }
+        if (n == 1) {
+            flag = true;
+            break;
+        }
+        if (s.find(n) != s.end()) {
+            break;
+        }
+        s.insert(n);
+    }
+    for (auto i : s) {
+        mp[i] = flag;
+        // cout << i << " ";
+    }
+}
+
+int main()
+{
+    // ifstream cin("input.txt");
+    // ofstream cout("output.txt");
+
+    // time start
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    for (int i = 1; i <= 100; i++) {
+        solve(i);
+    }
+
+    int count = 0;
+    
+    for (int i = 1; i <= 20; i++) {
+        if (mp[i] == 1) {
+            count++;
+            cout << i << " ";
+        }
+    }
+    cout << count;
+    
+}
