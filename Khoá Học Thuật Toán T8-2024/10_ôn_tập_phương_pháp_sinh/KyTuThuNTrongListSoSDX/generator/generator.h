05da93422804be4097533e0e506f06656bc823ec
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-10-09 05:46:51 +0700

    Revised implementation of KyTuThuNTrongListSoSDX/generator/generator.h

---
+++ Khoá Học Thuật Toán T8-2024/10_ôn_tập_phương_pháp_sinh/KyTuThuNTrongListSoSDX/generator/generator.h
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+#include "../lib/library.h"
+using namespace std;
+
+void gen(int iTest, int testnum, string target_file)
+{
+    ofstream cout(target_file);
+    
+    // Cout ra những input cần thiết
+
+     vector <int> lensub = {5, 9, 18};
+
+    string s = "";
+    int k = 1;
+
+    if (iTest * 100 < testnum * 20)
+    {
+        k = 0;
+        
+    }
+    else if (iTest * 100 < testnum * 40)
+    {
+        k = 1;
+    } else {
+        // Sinh test ngẫu nhiên
+       k = 2;
+    }
+
+    int len = lensub[k] * (iTest * 1.0 / testnum);
+    if (len == 0) len = 1;
+
+    s = random_string(1, "123456789") + random_string(len - 1, "0123456789");
+
+    long long n = stoll(s);
+
+    if (iTest >= testnum - 10)
+    {
+        n = 1e18- random(1, 100000);
+    }
+
+    if (iTest <= 10)
+    {
+        n = iTest * 3;
+    }
+
+    cout << n;
+    
+}
