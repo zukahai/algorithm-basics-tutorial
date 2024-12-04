68742f16a6084958c947bc7c8984d46190104329
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-12-04 20:46:11 +0700

    Resolved bugs in GanCo/KiemTraDayGiamDan/generator/solution.cpp

---
+++ HaiZuka - Phan Đức Hải/GanCo/KiemTraDayGiamDan/generator/solution.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ifstream cin("input.txt");
+    ofstream cout("output.txt");
+    // Bài giải của bạn ở dưới đây
+
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    int flag = 0;
+    for (int i = 1; i < n; i++) {
+        if (a[i] >= a[i - 1]) {
+            flag = 1;
+        }
+    }
+
+    if (flag == 0) {
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
+
+}
