25f37e71c62ebe04e703eb30fd896a84172961f3
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-12-10 17:44:45 +0700

    Added test.cpp for HSG9 problem set and removed obsolete HSG9.pdf file

---
+++ Phạm Thành An/11_làm_đề_thi_hsg_9/test.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    // cin >> n;
+    // vector<int> a(n);
+    // map<int, int> m;
+    // for (int i = 0; i < n; i++) {
+    //     cin >> a[i];
+    //     m[a[i]]++;
+    // }
+
+    // for (pair<int, int> p : m) {
+    //     cout << p.first << " " << p.second << endl;
+    // }
+
+    pair<int, int> p = {7, 10};
+    cout << p.first << " " << p.second << endl;
+
+    return 0;
+}
