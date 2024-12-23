742aa1e35156c6a9d0236751b289fc426e53ddfe
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-12-24 02:55:23 +0700

    Updated dependencies for HSG_24_12_2024/SYMMETRICARRAY/generator/SYMMETRICARRAY.cpp

---
+++ HaiZuka - Phan Đức Hải/HSG_24_12_2024/SYMMETRICARRAY/generator/SYMMETRICARRAY.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ifstream cin("SYMMETRICARRAY.INP");
+    ofstream cout("SYMMETRICARRAY.OUT");
+
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) 
+        cin >> a[i];
+
+    string ans = "YES";
+    for (int i = 0; i < n / 2; i++) {
+        if (a[i] != a[n - i - 1]) {
+            ans = "NO";
+            break;
+        }
+    }
+
+    cout << ans;
+}
