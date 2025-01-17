32b15a6dc42e10cbc15970437e8405c375182e05
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2025-01-17 19:01:33 +0700

    Implement binary search for optimal distance calculation in bai5_sub1.cpp

--- HaiZuka - Phan Đức Hải/DeThiHSG/GiaiDe07_01_2025/Bai5/bai5_sub1.cpp
+++ HaiZuka - Phan Đức Hải/DeThiHSG/GiaiDe07_01_2025/Bai5/bai5_sub1.cpp
@@ -2,6 +2,54 @@
 
 using namespace std;
 
+bool check(const string& s, int x) {
+    int n = s.size();
+    vector<int> positionsP;
+    vector<int> positionsStar;
+
+    for (int i = 0; i < n; ++i) {
+        if (s[i] == 'P') positionsP.push_back(i);
+        if (s[i] == '*') positionsStar.push_back(i);
+    }
+
+    int indexP = 0;
+    int indexStar = 0;
+
+    while (indexStar < positionsStar.size() && indexP < positionsP.size()) {
+        if (positionsP[indexP] > positionsStar[indexStar]) {
+            if (positionsP[indexP] - positionsStar[indexStar] > x) {
+                return false;
+            }
+            int minStar = positionsStar[indexStar];
+            while (indexStar < positionsStar.size() && positionsStar[indexStar] <= positionsP[indexP]) {
+                indexStar++;
+            }
+            if (indexStar == positionsStar.size()) {
+                return true;
+            }
+            while (indexStar < positionsStar.size()) {
+                int maxStar = positionsStar[indexStar];
+                int minD = min(abs(positionsP[indexP] - minStar), abs(positionsP[indexP] - maxStar));
+                int maxD = max(abs(positionsP[indexP] - minStar), abs(positionsP[indexP] - maxStar));
+                if (2 * minD + maxD <= x) {
+                    indexStar++;
+                    minD = min(abs(positionsP[indexP] - minStar), abs(positionsP[indexP] - maxStar));
+                    maxD = max(abs(positionsP[indexP] - minStar), abs(positionsP[indexP] - maxStar));
+                } else {
+                    break;
+                }
+            }
+
+        } else {
+            while (indexStar < positionsStar.size() && positionsStar[indexStar] - positionsP[indexP] <= x) {
+                indexStar++;
+            }
+        }
+        indexP++;
+    }
+    return indexStar >= positionsStar.size();
+}
+
 int main() {
     int n;
     cin >> n;
@@ -9,25 +57,16 @@ int main() {
     cin >> s;
     int res = 0;
 
-    for (int i = 0; i < n; i++) {
-        if (s[i] == 'P') {
-            int j = i;
-            while (j < n && s[j] != '*') {
-                j++;
-            }
-            if (j < n) {
-                res = max(res, (j - i));
-            }
-            j = i;
-            while (j >= 0 && s[j] != '*') {
-                j--;
-            }
-            if (j >= 0) {
-                res = max(res, (i - j));
-            }
-            
+    int l = 0;
+    int r = n;
+    while (l <= r) {
+        int mid = (l + r) / 2;
+        if (check(s, mid)) {
+            res = mid;
+            r = mid - 1;
+        } else {
+            l = mid + 1;
         }
     }
-
     cout << res;
 }
