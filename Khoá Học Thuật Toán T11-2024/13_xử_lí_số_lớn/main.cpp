fec1994618745d802f7da640447c2aa806de3bd4
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2025-01-18 21:14:39 +0700

    Add main program and mathematical formulas to readme for large number processing

---
+++ Khoá Học Thuật Toán T11-2024/13_xử_lí_số_lớn/main.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // int: -2^31 -> 2^31 - 1
+    // long long: -2^63 -> 2^63 - 1
+    int a = 100000;
+    long long b = (long long) a * a;
+    cout << b;
+}
