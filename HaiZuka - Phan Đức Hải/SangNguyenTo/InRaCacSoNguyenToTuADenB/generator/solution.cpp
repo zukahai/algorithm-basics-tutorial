ad91aebf17811ee1d1799fe3de00edd4baa8a6a2
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2025-03-01 05:11:28 +0700

    Improved performance in SangNguyenTo/InRaCacSoNguyenToTuADenB/generator/solution.cpp

---
+++ HaiZuka - Phan Đức Hải/SangNguyenTo/InRaCacSoNguyenToTuADenB/generator/solution.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int nMax = 10000000;
+
+vector<bool> isPrime(nMax + 1, true);
+
+void sang() {
+    isPrime[0] = isPrime[1] = false;
+    for (int i = 2; i * i <= nMax; i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= nMax; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+}
+
+int main()
+{
+    ifstream cin("input.txt");
+    ofstream cout("output.txt");
+    // Bài giải của bạn ở dưới đây
+    int a, b;
+    cin >> a >> b;
+    nMax = b;
+    sang();
+    int cnt = 0;
+    for (int i = a; i <= b; i++) {
+        if (isPrime[i]) {
+            cout << i << " ";
+        }
+    }
+    
+
+}
