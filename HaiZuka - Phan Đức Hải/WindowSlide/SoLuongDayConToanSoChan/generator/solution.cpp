1a23e12ddc423ce96de698eb23d67630fdbee0b2
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2025-03-02 05:38:40 +0700

    Improved performance in WindowSlide/SoLuongDayConToanSoChan/generator/solution.cpp

---
+++ HaiZuka - Phan Đức Hải/WindowSlide/SoLuongDayConToanSoChan/generator/solution.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    ifstream cin("input.txt");
+    ofstream cout("output.txt");
+    #endif
+    
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) 
+        cin >> a[i];
+    long long count = 0;
+    for (int i = 0; i < k; i++) 
+        if (a[i] % 2 == 0) 
+            count++;
+    int res = 0;
+    if (count == k) 
+        res ++;
+    for (int i = k; i < n; i++) 
+    {
+        if (a[i] % 2 == 0) 
+            count++;
+        if (a[i - k] % 2 == 0)
+            count--;
+        if (count == k)
+            res++;
+    }
+    cout << res;
+
+}
