7661430d4d0014515b530e6eaf251b8ae9242105
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2025-03-08 19:38:56 +0700

    Revised implementation of midcpp/SoMayMan/generator/generator.h

---
+++ HaiZuka - Phan Đức Hải/midcpp/SoMayMan/generator/generator.h
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#include "../lib/library.h"
+
+#define int long long
+using namespace std;
+
+vector<Subtask> subtasks = {
+    {percent: 20, lenN: 2, lenAi: 3}, //20% test có n <= 10^2, mỗi ai <= 10^3
+    {percent: 30, lenN: 4, lenAi: 6}, //30% test có n <= 10^4, mỗi ai <= 10^6
+    {percent: 50, lenN: 6, lenAi: 18}, //50% test có n <= 10^6, mỗi ai <= 10^18
+};
+
+
+void gen(int iTest, int testnum, string target_file)
+{
+    ofstream cout(target_file);
+    // Cout ra những đầu vào cần thiết ở phía dưới
+
+    int n = random(subtasks, iTest, testnum);
+    if (iTest % 2 == 0) {
+        if (random(0, 1) == 0) {
+            n = n * 10 + 4;
+        } else {
+            n = n * 10 + 7;
+        }
+    }
+
+    cout << n << '\n';
+    
+}
