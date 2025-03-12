4d8bf0689ed214473587fe22d2d31763cd4c2053
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2025-03-13 04:36:09 +0700

    Optimized algorithms in MinhTri/BAI1/generator/generator.h

---
+++ HaiZuka - Phan Đức Hải/MinhTri/BAI1/generator/generator.h
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#include "../lib/library.h"
+
+#define int long long
+using namespace std;
+
+vector<Subtask> subtasks = {
+    {percent: 20, lenN: 2, lenAi: 3}, //20% test có n <= 10^2, mỗi ai <= 10^3
+    {percent: 30, lenN: 2, lenAi: 6}, //30% test có n <= 10^4, mỗi ai <= 10^6
+    {percent: 50, lenN: 3, lenAi: 9}, //50% test có n <= 10^6, mỗi ai <= 10^18
+};
+
+
+void gen(int iTest, int testnum, string target_file)
+{
+    ofstream cout(target_file);
+    // Cout ra những đầu vào cần thiết ở phía dưới
+
+    vector<int> a = random_vector(subtasks, iTest, testnum);
+    cout << a.size() << endl;
+    for (int i = 0; i < a.size(); i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+
+    
+}
