a10f3c9fd0dfa4c6a369d0e180f13d5deb2ca7b6
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2025-03-13 04:36:42 +0700

    Reorganized project structure with MinhTri/BAI4/generator/solution.cpp

---
+++ HaiZuka - Phan Đức Hải/MinhTri/BAI4/generator/solution.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // Không phải online judge sẽ đọc file input.txt, output.txt
+    #ifndef ONLINE_JUDGE
+    ifstream cin("input.txt");
+    ofstream cout("output.txt");
+    #endif
+    // Bài giải của bạn ở dưới đây
+    
+
+
+}
